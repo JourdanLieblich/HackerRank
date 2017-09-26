66d140492f2ccce234d8e69f58cdde0515648786
JourdanLieblich <jslieblich@gmail.com>
2017-09-25 17:05:59 -0700

    Java Challenge

--- mcCandles.c
+++ mcCandles.c
@@ -30,6 +30,7 @@ int main() {
     int n; 
     scanf("%i", &n);
     int *ar = malloc(sizeof(int) * n);
+    int * ar = malloc(sizeof(int)* n);
     for(int ar_i = 0; ar_i < n; ar_i++){
        scanf("%i",&ar[ar_i]);
     }
